b34b589f20a874d0444e0b77c0c9026bb5f709b1
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2022-12-03 11:56:42 +0530

    Time: 17 ms (13.63%), Space: 42.7 MB (17.95%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,31 +1,13 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-      List<Character> set =new ArrayList<>();
-        set.add('a');
-        set.add('A');
-        set.add('e');
-        set.add('E');
-        set.add('i');
-        set.add('I');
-        set.add('o');
-        set.add('O');
-        set.add('u');
-        set.add('U');
-        set.add('U');
-        set.add('i');
-        set.add('I');
-        set.add('o');
-        set.add('O');
-        set.add('u');
-        set.add('U');
-        set.add('U');
-		//find the mid
-        int mid = s.length() / 2;
-        int count = 0;
-        //increment the count for left half, decrement count for the second half if its a vowel
-		for (int i = 0; i < s.length(); i++)
-            count += (set.contains(s.charAt(i))) ? ((i < mid) ? 1 : -1) : 0; 
-        //finally count should be 0 to match left and right half
-		return count == 0;
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        int vowelsCount = 0, midIndex = s.length() / 2;
+
+        for (int i = 0; i < midIndex; i++) {
+           
+            if (vowels.contains(s.charAt(i))) vowelsCount++;
+            if (vowels.contains(s.charAt(midIndex + i))) vowelsCount--;
+        }
+        return vowelsCount == 0;
     }
 }

2f5e78b76f696e0a89b1ee42cf3e13cb2fe6f306
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2022-12-03 12:16:13 +0530

    Time: 16 ms (15.98%), Space: 42.6 MB (31.39%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int c1=0;
+        int c1=0,mid=s.length()/2;
         Set<Character> set = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
         for(int i=0;i<s.length();i++)
         {
-            if(set.contains(s.charAt(i)) )
+            char a=s.charAt(i);
+            if(set.contains(a) )
             {         
-                if(i<s.length()/2)
-            {
-                c1++;
+                if(i<mid)
+                {
+                    c1++;
+                }
+                else
+                {
+                    c1--;
+                }
             }
-            else
-            {
-                c1--;
-            }
-        }
         }
         
        return (c1==0);

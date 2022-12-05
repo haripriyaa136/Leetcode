af41eddd869144fddb44588eeeb9f726c1224003
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2022-12-05 11:01:56 +0530

    Time: 178 ms (16.87%), Space: 74.8 MB (5.11%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,29 +1,23 @@
 class Solution {
-    private int N = 26;
-    public boolean closeStrings(String word1, String word2) {
-		// count the English letters
-        int[] arr1 = new int[N], arr2 = new int[N];
-        for (char ch : word1.toCharArray())
-            arr1[ch - 'a']++;
-        for (char ch : word2.toCharArray())
-            arr2[ch - 'a']++;
-		
-		// if one has a letter which another one doesn't have, dont exist
-        for (int i = 0; i < N; i++) {
-            if (arr1[i] == arr2[i]) {
-                continue;
-            }
-            if (arr1[i] == 0 || arr2[i] == 0) {
-                return false;
-            }
-        }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for (int i = 0; i < N; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+   public boolean closeStrings(String word1, String word2) {
+	if (word1.length() != word2.length()) {
+		return false;
+	}
+	Map<Character, Integer> word1Map = new HashMap<>();
+	Map<Character, Integer> word2Map = new HashMap<>();
+	for (char c : word1.toCharArray()) {
+		word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
+	}
+	for (char c : word2.toCharArray()) {
+		word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
+	}
+	if (!word1Map.keySet().equals(word2Map.keySet())) {
+		return false;
+	}
+	List<Integer> word1FrequencyList = new ArrayList<>(word1Map.values());
+	List<Integer> word2FrequencyList = new ArrayList<>(word2Map.values());
+	Collections.sort(word1FrequencyList);
+	Collections.sort(word2FrequencyList);
+	return word1FrequencyList.equals(word2FrequencyList);
+}
 }

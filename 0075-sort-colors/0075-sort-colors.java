e5d97f26268a262ecdd77a33faebd46b36d5d927
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-03 09:23:57 +0530

    Time: 0 ms (100%), Space: 41.3 MB (8.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,35 +2,39 @@ class Solution {
     public void sortColors(int[] nums) {
         int s=0,e=nums.length-1;
         //  [0, 0, 2, 1, 1, 2]
-        int i=0;
-        while((i <= e) && (s < e)){
+        for(int i=0;((i<nums.length) && (i<=e));){
             
-            
+            if(nums[e]==2){
+                e--;
+            }
             if(nums[i]==0){
-                
-                
-                nums[i]=nums[s];
-                nums[s]=0;
-                //System.out.println("Arrray after swap"+nums[i]+" "+ nums[s]);
-               // System.out.println(Arrays.toString(nums));
+                nums=swap(nums,i,s);
+                // System.out.println("Arrray after swap"+nums[i]+" "+ nums[s]);
+                // System.out.println(Arrays.toString(nums));
                 s++;
-                i++;  
+                i++;
+                
               
             }
-           else if(nums[i]==2){
-                
-                nums[i]=nums[e];
-                nums[e]=2;
+           else if(nums[i]==2 && (i<e) && (nums[e]!=2)){
+                nums=swap(nums,i,e);
+                // System.out.println("Arrray after swap"+nums[i]+" "+ nums[s]);System.out.println(Arrays.toString(nums));
                 e--;
+               
             }
             else if(nums[i]==1){
                 i++;
             }
-           System.out.println("i "+i+" s "+s+" e "+e);
-           System.out.println(Arrays.toString(nums));
-
+           
 
         }
     }
-   
+    public int[] swap(int[] arr,int i,int j){
+
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        return arr;
+
+    }
 }

15e6c07e7ce31e024e4bee75e63e82460bd89b44
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-06 17:18:08 +0530

    Time: 3 ms (39.45%), Space: 49.5 MB (73.77%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[][] sortTheStudents(int[][] grid, int k) {
          for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
-                if(grid[i][k]>grid[j][k]){
+            for(int j=i+1;j<grid.length;j++){
+                if(grid[i][k]<grid[j][k]){
+
                     int[] temp=grid[i];
                     grid[i]=grid[j];
                     grid[j]=temp;

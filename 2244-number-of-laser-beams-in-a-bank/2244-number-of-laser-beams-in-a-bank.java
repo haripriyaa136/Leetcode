9f642be736a0960b50b841d15ed144881f946002
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-08 20:44:06 +0530

    Time: 17 ms (40.18%), Space: 43.5 MB (95.58%) - LeetHub

---
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int countOfoneRow;
+        int k=0,laserBeam=0;
+        int arr[]=new int[2];
+        for(int i=0;i<bank.length;i++){
+          countOfoneRow = calCountOne(bank[i]);
+          if(countOfoneRow!=0){
+              arr[k]=countOfoneRow;
+              k++;
+              if(k==2){
+              laserBeam+=(arr[0]*arr[1]);
+              k=1;
+              arr[0]=arr[1];
+              arr[1]=0;
+          }
+        }
+          
+
+        }
+        return laserBeam;
+    }
+    public int calCountOne(String str){
+        int c=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='1'){
+                c++;
+            }
+        }
+        return c;
+    }
+}

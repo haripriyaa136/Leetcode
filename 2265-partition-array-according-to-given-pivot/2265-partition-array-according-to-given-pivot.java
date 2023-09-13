98128c7ac497db2993b85ab4859e65b96c231526
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-13 16:20:46 +0530

    Time: 5 ms (74.08%), Space: 63.7 MB (17.08%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,27 +1,40 @@
 class Solution {
-        public int[] pivotArray(int[] nums, int pivot) {
-        
-        int n = nums.length, pivotFreq = 0, i = 0;
-        int[] ans = new int[n];
-        
-        for (int num : nums) {
-            if (num < pivot) {
-                ans[i++] = num;
-            }else if (num == pivot) {
-                ++pivotFreq;
-            } 
-        }
-        
-        while (pivotFreq-- > 0) {
-            ans[i++] = pivot;
-        }
-        
-        for (int num : nums) {
-            if (num > pivot) {
-                ans[i++] = num;
+    public int[] pivotArray(int[] nums, int pivot) {
+        int l=0,g=0,e=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<pivot){
+                l++;
+            }
+            else if(nums[i]>pivot){
+                g++;
+            }
+            else{
+                e++;
             }
         }
-        
-        return ans;
+        // System.out.println("l "+l);
+        // System.out.println("g "+g+" new g "+(e+l+1));
+        // System.out.println("e "+e+" new e "+(l+1));
+        g=e+l;
+        e=l;
+        l=0;
+        int arr[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            // System.out.println("Now nums "+nums[i]);
+            if(nums[i]<pivot){
+                arr[l]=nums[i];
+                l++;
+            }
+            else if(nums[i]>pivot){
+                arr[g]=nums[i];
+                g++;
+            }
+            else{
+                arr[e]=pivot;
+                e++;
+            }
+          //  System.out.println("l and e and g "+l+""+e+""+g);
+        }
+        return arr;
     }
 }

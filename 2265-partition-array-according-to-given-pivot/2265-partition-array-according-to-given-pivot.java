0dbd42c706aaf44c5a0907357cf3fa087defe6b3
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-13 16:34:30 +0530

    Time: 5 ms (74.08%), Space: 63.5 MB (28.17%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,40 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int l=0,g=0,e=0;
-        for(int i=0;i<nums.length;i++){
+        int s=0,e=nums.length-1;
+        int ans[]=new int[nums.length];
+        for(int i=0,j=nums.length-1;i<nums.length;i++,j--){
             if(nums[i]<pivot){
-                l++;
+                ans[s]=nums[i];
+                s++;
             }
-            else if(nums[i]>pivot){
-                g++;
-            }
-            else{
-                e++;
+            if(nums[j]>pivot){
+                ans[e]=nums[j];
+                e--;
             }
         }
-        // System.out.println("l "+l);
-        // System.out.println("g "+g+" new g "+(e+l+1));
-        // System.out.println("e "+e+" new e "+(l+1));
-        g=e+l;
-        e=l;
-        l=0;
-        int arr[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            // System.out.println("Now nums "+nums[i]);
-            if(nums[i]<pivot){
-                arr[l]=nums[i];
-                l++;
-            }
-            else if(nums[i]>pivot){
-                arr[g]=nums[i];
-                g++;
-            }
-            else{
-                arr[e]=pivot;
-                e++;
-            }
-          //  System.out.println("l and e and g "+l+""+e+""+g);
+        while(s<=e){
+            ans[s]=pivot;
+            s++;
         }
-        return arr;
+        return ans;
     }
 }

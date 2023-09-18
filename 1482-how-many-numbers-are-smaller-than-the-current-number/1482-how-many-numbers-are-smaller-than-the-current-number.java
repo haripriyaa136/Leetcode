a70c5e8eeecbcb6512e44165492e4acf338deaf4
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-18 10:29:16 +0530

    Time: 4 ms (87.36%), Space: 43.5 MB (69.04%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,15 +1,34 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] res= new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if((i!=j)&&(nums[j]<nums[i])){
-                    res[i]++;
-                }
-            }
-        }
+        // int[] res= new int[nums.length];
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=0;j<nums.length;j++){
+        //         if((i!=j)&&(nums[j]<nums[i])){
+        //             res[i]++;
+        //         }
+        //     }
+        // }
 
-        return res;
-        
+        // return res;
+
+
+        int[] count = new int[101];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]]++;
+        }
+        for(int i=1;i<count.length;i++){
+            count[i]+=count[i-1];
+        }
+        System.out.println(Arrays.toString(count));
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=count[nums[i]-1]; 
+            }
+           
+        }
+        return nums;
     }
 }

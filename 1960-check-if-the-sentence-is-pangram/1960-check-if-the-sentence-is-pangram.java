5b1fb264dc0a8ba0635bfed81cffde9bb9e1e115
HARIPRIYAA <98803642+haripriyaa136@users.noreply.github.com>
2023-09-19 12:05:53 +0530

    Time: 0 ms (100%), Space: 40.2 MB (97.08%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean[] letters  = new boolean[26];
-        
-        for(char c : sentence.toCharArray()) {
-            letters[c - 'a'] = true;
-        }
-        
-        //find any letter that not exist
-        for(boolean existLetter : letters) {
-            if(!existLetter) return false;
-        }
-        
-        return true;
+// I tried using hashset - I got 3 ms , 
+//  Next approach : by traversing through all characters , with array of
+//  boolean where we store the array element as true if the char is present
+// boolArray[ch-'a']= true  
+// Then traverse through the boolean array and check if any element is false 
+   // => Third Approach :
+   for(char ch ='a';ch<='z';ch++){
+       if(!sentence.contains(String.valueOf(ch))){
+           return false;
+       }
+   }
+   
+   return true;
+   
     }
 }
